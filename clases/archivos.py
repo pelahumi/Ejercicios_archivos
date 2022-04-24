149176bc673acf12296c8fb6355cfe39ae1a0c19
Pelayo <phuermij@myuax.com>
2022-04-24 12:00:14 +0200

    ordenar por apellidos

--- clases/archivos.py
+++ clases/archivos.py
@@ -9,3 +9,17 @@ class Notas():
     
     def ejercicio1(self):
         data = pd.read_csv(self.archivo) #Leemos el archivo csv
+
+        lista_diccionarios = []
+        #Creamos una columna que contenga el nombre y apellidos
+        data["Nombre completo"] = data["Apellidos"] + ", " + data["Nombre"]
+        data.drop('Apellidos', inplace=True, axis=1) 
+        data.drop('Nombre', inplace=True, axis=1) 
+
+        #Ordenamos las filas por apellidos
+        data = data.sort_values("Nombre completo")
+
+        #Hacemos una lista con cada columna del csv
+        parcial1 = list(data["Parcial1"])
+        parcial2 = list(data["Parcial2"])
+
